c53692819c5e2bd27f98f5c37de94e170cad5638
Matthieu Bricaire <matthieu.bricaire@ensae.fr>
2024-01-13 11:23:00 +0100

    Refactored vgg16 code

--- vgg16.py
+++ vgg16.py
@@ -8,66 +8,35 @@ class Vgg16(nn.Module):
         self.mode = mode
         features = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1).features
         self.feat = features
-        self.to_relu_1_1 = nn.Sequential()
-        self.to_relu_1_2 = nn.Sequential()
-        self.to_relu_2_1 = nn.Sequential()
-        self.to_relu_2_2 = nn.Sequential()
-        self.to_relu_3_1 = nn.Sequential()
-        self.to_relu_3_3 = nn.Sequential()
-        self.to_relu_4_1 = nn.Sequential()
-        self.to_relu_4_2 = nn.Sequential()
-        self.to_relu_4_3 = nn.Sequential()
-        self.to_relu_5_1 = nn.Sequential()
 
-        for x in range(2):
-            self.to_relu_1_1.add_module(str(x), features[x])
-        for x in range(2, 4):
-            self.to_relu_1_2.add_module(str(x), features[x])
-        for x in range(4, 7):
-            self.to_relu_2_1.add_module(str(x), features[x])
-        for x in range(7, 9):
-            self.to_relu_2_2.add_module(str(x), features[x])
-        for x in range(9, 12):
-            self.to_relu_3_1.add_module(str(x), features[x])
-        for x in range(12, 16):
-            self.to_relu_3_3.add_module(str(x), features[x])
-        for x in range(16, 19):
-            self.to_relu_4_1.add_module(str(x), features[x])
-        for x in range(19, 21):
-            self.to_relu_4_2.add_module(str(x), features[x])
-        for x in range(21, 23):
-            self.to_relu_4_3.add_module(str(x), features[x])
-        for x in range(23, 26):
-            self.to_relu_5_1.add_module(str(x), features[x])
-        
-        # don't need the gradients, just want the features
+        self.relu_layers = nn.ModuleList()
+        layer_indices = [0, 2, 4, 7, 9, 12, 16, 19, 21, 23, 26]
+        self.relu_names = ['1_1', '1_2', '2_1', '2_2', '3_1', '3_3', '4_1', '4_2', '4_3', '5_1']
+
+        for i in range(len(layer_indices) - 1):
+            layer_start, layer_end = layer_indices[i], layer_indices[i + 1]
+            setattr(self, f'to_relu_{self.relu_names[i]}', nn.Sequential(*features[layer_start:layer_end]))
+
+        if self.mode == 'generative_st':
+            self.extracted_fmaps = ['1_2', '2_2', '3_3', '4_3']
+        else:
+            self.extracted_fmaps = ['1_1', '2_1', '3_1', '4_1', '4_2', '5_1']
+
+        # Freeze parameters
         for param in self.parameters():
             param.requires_grad = False
 
     def forward(self, x):
-        h = self.to_relu_1_1(x)
-        h_relu_1_1 = h
-        h = self.to_relu_1_2(h)
-        h_relu_1_2 = h
-        h = self.to_relu_2_1(h)
-        h_relu_2_1 = h
-        h = self.to_relu_2_2(h)
-        h_relu_2_2 = h
-        h = self.to_relu_3_1(h)
-        h_relu_3_1 = h
-        h = self.to_relu_3_3(h)
-        h_relu_3_3 = h
-        h = self.to_relu_4_1(h)
-        h_relu_4_1 = h
-        h = self.to_relu_4_2(h)
-        h_relu_4_2 = h
-        h = self.to_relu_4_3(h)
-        h_relu_4_3 = h
+        feature_maps = []
 
-        if self.mode == 'generative_st':
-            return (h_relu_1_2, h_relu_2_2, h_relu_3_3, h_relu_4_3)
+        for i in range(len(self.relu_names)):
+            layer_idx = self.relu_names[i]
+            x = getattr(self, f'to_relu_{layer_idx}')(x)
+            if layer_idx in self.extracted_fmaps:
+                feature_maps.append(x)
+            
+            if (self.mode == 'generative_st') and (len(feature_maps) == len(self.extracted_fmaps)):
+                return tuple(feature_maps)
         
-        h = self.to_relu_5_1(h)
-        h_relu_5_1 = h
-
-        return (h_relu_1_1, h_relu_2_1, h_relu_3_1, h_relu_4_1, h_relu_5_1, h_relu_4_2)
+        feature_maps[-2], feature_maps[-1] = feature_maps[-1], feature_maps[-2]
+        return tuple(feature_maps)
