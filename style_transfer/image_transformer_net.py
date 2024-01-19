10a0ebd3348e6c3e1797b196d75af1db6573f70c
RTalate <rayan.talate@ensae.fr>
2024-01-19 19:39:53 +0100

    adding comments

--- style_transfer/image_transformer_net.py
+++ style_transfer/image_transformer_net.py
@@ -1,21 +1,21 @@
-"""Image transformer network."""
+"""Image transformer network, which turns our input image into a stylized image."""
 import torch.nn as nn
 import torch
 
-# Conv Layer
+# Class of the convolutional layers that will serve as our encoding layers in the imagetransformnet class.
 class ConvLayer(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride):
         super(ConvLayer, self).__init__()
         padding = kernel_size // 2
         self.reflection_pad = nn.ReflectionPad2d(padding)
-        self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride) #, padding)
+        self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride) 
 
     def forward(self, x):
         out = self.reflection_pad(x)
         out = self.conv2d(out)
         return out
 
-# Upsample Conv Layer
+# Class of the Upsample convolutional layers that will serve as our decoding layers in the imagetransformnet class
 class UpsampleConvLayer(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
         super(UpsampleConvLayer, self).__init__()
@@ -35,8 +35,7 @@ class UpsampleConvLayer(nn.Module):
 
 # Residual Block
 #   adapted from pytorch tutorial
-#   https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/02-
-#   intermediate/deep_residual_network/main.py
+#   https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/02-intermediate/deep_residual_network/main.py
 class ResidualBlock(nn.Module):
     def __init__(self, channels):
         super(ResidualBlock, self).__init__()
@@ -54,44 +53,45 @@ class ResidualBlock(nn.Module):
         out = self.relu(out)
         return out 
 
-# Image Transform Network
+# Image Transform Network, which will be used to transform our input into a stylized image. See our report for more details. 
 class ImageTransformNet(nn.Module):
     def __init__(self):
         super(ImageTransformNet, self).__init__()
         
-        # nonlineraity
+        # nonlinearity, which tends to make the model a bit more complex and efficient.
+        # ReLU converts an input into its positive part. Tanh is the hyperbolic tangent. 
         self.relu = nn.ReLU()
         self.tanh = nn.Tanh()
 
-        # encoding layers
-        self.conv1 = ConvLayer(3, 32, kernel_size=9, stride=1)
-        self.in1_e = nn.InstanceNorm2d(32, affine=True)
+        # encoding layers, which increase the size of the output.
+        self.conv1 = ConvLayer(3, 32, kernel_size=9, stride=1) #Layer "Conv 1" of our report.
+        self.in1_e = nn.InstanceNorm2d(32, affine=True) # instance normalization
 
-        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
-        self.in2_e = nn.InstanceNorm2d(64, affine=True)
+        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2) #Layer "Conv 2" of our report.
+        self.in2_e = nn.InstanceNorm2d(64, affine=True) #instance normalization
 
-        self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
-        self.in3_e = nn.InstanceNorm2d(128, affine=True)
+        self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2) #Layer "Conv 3" of our report.0
+        self.in3_e = nn.InstanceNorm2d(128, affine=True) #instance normalization
 
-        # residual layers
+        # residual layers.
         self.res1 = ResidualBlock(128)
         self.res2 = ResidualBlock(128)
         self.res3 = ResidualBlock(128)
         self.res4 = ResidualBlock(128)
         self.res5 = ResidualBlock(128)
 
-        # decoding layers
-        self.deconv3 = UpsampleConvLayer(128, 64, kernel_size=3, stride=1, upsample=2 )
+        # decoding layers, which reduce the size of the output.
+        self.deconv3 = UpsampleConvLayer(128, 64, kernel_size=3, stride=1, upsample=2 )#Layer "Deconv 1" of our report.
         self.in3_d = nn.InstanceNorm2d(64, affine=True)
 
-        self.deconv2 = UpsampleConvLayer(64, 32, kernel_size=3, stride=1, upsample=2 )
+        self.deconv2 = UpsampleConvLayer(64, 32, kernel_size=3, stride=1, upsample=2 )#Layer "Deconv 1" of our report.
         self.in2_d = nn.InstanceNorm2d(32, affine=True)
 
-        self.deconv1 = UpsampleConvLayer(32, 3, kernel_size=9, stride=1)
+        self.deconv1 = UpsampleConvLayer(32, 3, kernel_size=9, stride=1)#Layer "Deconv 3" of our report.
         self.in1_d = nn.InstanceNorm2d(3, affine=True)
 
     def forward(self, x):
-        # encode
+        # encode : we add the ReLU activation as stated in our report.
         y = self.relu(self.in1_e(self.conv1(x)))
         y = self.relu(self.in2_e(self.conv2(y)))
         y = self.relu(self.in3_e(self.conv3(y)))
@@ -103,15 +103,10 @@ class ImageTransformNet(nn.Module):
         y = self.res4(y)
         y = self.res5(y)
 
-        # decode
+        # decode : we add the ReLU activation as stated in our report.
         y = self.relu(self.in3_d(self.deconv3(y)))
         y = self.relu(self.in2_d(self.deconv2(y)))
         #y = self.tanh(self.in1_d(self.deconv1(y)))
         y = self.deconv1(y)
 
         return y
-
-# transfo = ImageTransformNet()
-# a = torch.rand(1, 3, 256, 256)
-# output = transfo(a)
-# print(5)
